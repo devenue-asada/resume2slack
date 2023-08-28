ff3ee5a85cafc896d485fd3d751e4777ffcb311d
devenue-asada <kiyofumi.asada@devenues.com>
2023-08-28 11:58:50 +0900

    modify

--- main.py
+++ main.py
@@ -1,29 +1,10 @@
 import requests
 
-token = "xoxb-3538245060513-5821303347312-wmyPLGlz1btNYgI5dt3DqSnv"
+token = "xoxb-3538245060513-5821303347312-cnaQBouGIMlwFJaNPDkMYznr"
 CHANNEL = "#pj"
 
-#####################################
-# 画像を生成する例、アップロードするだけなら不要
-#####################################
-
-# # データ読み込む
-# data = pd.read_table(
-#     "/path/to/data/input.tsv", #なんかtsv/csv読み込むサンプル
-#     header=-1,
-#     names=("date","value")
-# )
-# # 軸の基準になるとこ
-# data.index = pd.to_datetime(data.iloc[:,0])
-# data.plot()
-# # 保存するよ
-# plt.savefig('figure.png')
-
-###############
-# 画像送信ここから
-###############
 headers = {
-    'Authorization': 'Bearer xoxb-3538245060513-5821303347312-wmyPLGlz1btNYgI5dt3DqSnv',
+    'Authorization': f'Bearer {token}',
 }
 
 files = {
@@ -33,5 +14,4 @@ files = {
 
 print(open('./hoge.png', 'rb'))
 
-response = requests.post('https://slack.com/api/files.upload', headers=headers, files=files)
-print(res)
+requests.post('https://slack.com/api/files.upload', headers=headers, files=files)
